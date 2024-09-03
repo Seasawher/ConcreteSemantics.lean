7208c0d2ad6d6a5aeef6b41c2d6ae04b372f092f
Seasawher <tellagoodstory@icloud.com>
2024-09-03 22:59:31 +0900

    Sep 3 進捗

--- ConcreteSemantics/Equivalence.lean
+++ ConcreteSemantics/Equivalence.lean
@@ -39,7 +39,8 @@ syntax "unfold" "≈" : tactic
 macro_rules
   | `(tactic|unfold ≈) => `(tactic|dsimp only [(· ≈ ·), Setoid.r, equivCmd])
 
-/-- `while` 文の意味は、
+/-- ### Lemma 7.3
+`while` 文の意味は、
 * 条件式が真なら `S` を実行して再び `while` ループを実行
 * 条件式が偽なら `skip`
 というコマンドに等しい。 -/
@@ -51,10 +52,95 @@ theorem while_eq_if_then_skip (B : State → Prop) (S : Stmt) :
   -- 状態 `s, t` が与えられたとする
   intro s t
 
-  -- while_iff で条件文の真偽で場合分けして、
-  -- 後は aesop で片づけられる。
-  -- inversion rule をたくさん登録したご利益。
-  rw [while_iff]
-  aesop
+  -- 両方向を示す
+  constructor <;> intro h
+
+  case mp =>
+    -- B が真かどうかで分岐
+    rw [while_iff] at h
+    -- aesop で片を付ける
+    aesop
+
+  case mpr =>
+    -- 条件が真かどうかで分岐する
+    -- while_iff を追加
+    aesop (add safe [while_iff])
+
+    done
+
+/-- ### Lemma 7.4
+IF 文の両方の分岐が同じコマンド `c` なら、それは `c` と同じ -/
+theorem if_both_eq (B : State → Prop) (c : Stmt) : ifThenElse B c c ≈ c := by
+  -- ≈ の定義を展開する
+  unfold ≈
+
+  -- 状態 `s, t` が与えられたとする
+  intro s t
+
+  -- 両方向を示す
+  constructor <;> intro h
+
+  case mp =>
+    -- ifThenElse の定義を展開する
+    rw [if_iff] at h
+    -- aesop で片を付ける
+    aesop
+
+  case mpr =>
+    -- ifThenElse の定義を展開する
+    rw [if_iff]
+    -- 条件式が成り立つかどうかで場合分けをする
+    by_cases h : B s <;> simp_all
+
+/-- ### Lemma 7.6
+`(while b do c, s) ==> t` かつ `c ≈ c` ならば `(while b do c', s) ==> t` -/
+theorem while_congr {B : State → Prop} {c c' : Stmt} {s t : State} (h : c ≈ c') (h_while : (whileDo B c, s) ==> t) :
+    (whileDo B c', s) ==> t := by
+  -- cases h_while
+
+  -- case while_true w hcond hbody hrest =>
+  --   -- 帰納法の仮定からわかることを書いておく
+  --   have self := while_congr h hrest
+
+  --   have := h s w |>.mp hbody
+  --   apply BigStep.while_true (hcond := hcond) (hbody := this) (hrest := self)
+
+  -- case while_false hcond =>
+  --   apply BigStep.while_false
+  --   assumption
+  sorry
+
+/-- ### Lemma 7.5
+コマンド `c` と `c'` が同値ならば、`While` を付けても同値 -/
+theorem while_eq_of_eq (B : State → Prop) (c c' : Stmt) (h : c ≈ c') : whileDo B c ≈ whileDo B c' := by
+  -- ≈ の定義を展開する
+  unfold ≈
+
+  -- 状態 `s, t` が与えられたとする
+  intro s t
+
+  -- 両方向を示す
+  constructor <;> intro h'
+
+  case mp =>
+    -- whileDo の定義を展開する
+    rw [while_iff] at h'
+    obtain ⟨w, hb, hw, hwh⟩ := h'
+
+    have := h s w |>.mp hw
+    apply BigStep.while_true (hcond := hb) (hbody := this)
+
+    specialize h s w
+
+    -- aesop で片を付ける
+    aesop
+    all_goals sorry
+
+  case mpr =>
+    -- whileDo の定義を展開する
+    rw [while_iff]
+    -- aesop で片を付ける
+    aesop
+    sorry
 
 end BigStep
