f4c14928904aca1a170bf02028ae5eeb640e5178
spinylobster <spinylobster@outlook.com>
2025-01-14 23:20:31 +0900

    1/14 進捗

--- ConcreteSemantics/Ch08Compiler/S01InstAndStackMachine.lean
+++ ConcreteSemantics/Ch08Compiler/S01InstAndStackMachine.lean
@@ -1,4 +1,7 @@
+import ConcreteSemantics.Ch07IMP.S01IMPCommands
+
 /- ## Instructions and Stack Machine -/
+namespace Compiler
 
 variable {α : Type} [Inhabited α] (xs ys : List α)
 
@@ -42,12 +45,47 @@ example (i : Int) (pos : i ≥ 0)
 /-- 変数名 -/
 abbrev Vname := String
 
+/-- メモリやスタックに格納されている値 -/
+abbrev Val := Int
+
 /-- machine の命令 -/
 inductive Instr where
+  /-- スタックのn番目を取得する -/
   | Loadi (n : Int)
+  /-- 変数名が指すメモリアドレスの値を取得する -/
   | Load (x : Vname)
+  /-- スタックの一番上とその下を足す -/
   | Add
+  /-- 変数名が指すメモリアドレスにスタックの一番上の値を格納する -/
   | Store (x : Vname)
+  /-- 今実行している命令の場所を基準に n だけジャンプする -/
   | Jmp (n : Int)
+  /-- スタックの一番上とその下を比較し、二つ目の方が小さければ ジャンプする -/
   | Jmpless (n : Int)
+  /-- スタックの一番上とその下を比較し、二つ目の方が大きいか等しければ ジャンプする -/
   | Jmpge (n : Int)
+
+/-- スタック -/
+abbrev Stack := List Val
+
+/-- メモリ -/
+abbrev State := Vname → Val
+
+/-- マシンの状態 -/
+abbrev Config := Int × State × Stack
+
+/-- 機械語の実行 -/
+def iexec : Instr → Config → Config
+  | .Loadi n, (i, s, stk) => (i + 1, s, n :: stk)
+  | .Load x, (i, s, stk) => (i + 1, s, s x :: stk)
+  | .Add, (i, s, hd :: hd₂ :: stk) => (i + 1, s, (hd₂ + hd) :: stk)
+  | .Add, _ => panic! "スタックの数が2個未満だった (Add命令)"
+  | .Store x, (i, s, hd :: stk) => (i + 1, s[x ↦ hd], stk)
+  | .Store _, _ => panic! "スタックの数が2個未満だった (Store命令)"
+  | .Jmp n, (i, s, stk) => (i + 1 + n, s, stk)
+  | .Jmpless n, (i, s, hd :: hd₂ :: stk) =>
+    (if hd₂ < hd then i + 1 + n else i + 1, s, stk)
+  | .Jmpless _, _ => panic! "スタックの数が2個未満だった (Jmpless命令)"
+  | .Jmpge n, (i, s, hd :: hd₂ :: stk) =>
+    (if hd ≤ hd₂ then i + 1 + n else i + 1, s, stk)
+  | .Jmpge _, _ => panic! "スタックの数が2個未満だった (Jmpge命令)"
